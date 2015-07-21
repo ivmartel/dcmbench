10afb712079e3daacad2c83d5253ebb0d048fcc9
ivmartel <ivmartel@gmail.com>
2015-07-22 00:10:20 +0200

    Massive restructuring...

--- src/dicomDiff.js
+++ src/dicomDiff.js
@@ -56,6 +56,24 @@ DicomDiff = function (f1, f2, dataList) {
     // status
     setStatus("running");
 
+    // html display
+    var preId = "diff-results" + dataIndex;
+
+    var button = document.createElement("button");
+    button.onclick = function() {
+      onShowButton(preId);
+    };
+    button.id = preId + "-show";
+    button.appendChild(document.createTextNode(data.name));
+
+    var pre = document.createElement("pre");
+    pre.id = preId
+    pre.style.display = "none";
+
+    var div = document.getElementById("diff-results");
+    div.appendChild(button);
+    div.appendChild(pre);
+
     // real work
     var request = new XMLHttpRequest();
     request.open('GET', data.url, true);
@@ -79,20 +97,25 @@ DicomDiff = function (f1, f2, dataList) {
         else {
           nSame += part.value.length;
         }
-        // green for additions, red for deletions
+        // limegreen for additions, crimson for deletions
         // grey for common parts
-        var color = part.added ? 'green' :
-          part.removed ? 'red' : 'grey';
+        var color = part.added ? 'white' :
+          part.removed ? 'white' : 'grey';
+        var bgcolor = part.added ? 'limegreen' :
+          part.removed ? 'crimson' : 'white';
         var span = document.createElement('span');
         span.style.color = color;
-        span.appendChild(document
-          .createTextNode(part.value));
-        document.getElementById("diff-results").appendChild(span);
+        span.style.background = bgcolor;
+        span.appendChild( document.createTextNode(part.value) );
+        pre.appendChild(span);
       });
 
+      // similarity percentage
       var total = nSame + nDiff;
-      var percent = nDiff * 100 / total;
-      console.log("Difference (%): " + percent );
+      var percent = nSame * 100 / total;
+      var diffText = document.createElement("p");
+      diffText.appendChild(document.createTextNode(percent.toPrecision(4) + "% similar."));
+      div.appendChild(diffText);
 
       // check status
       if ( self.getStatus() !== "cancelled" ) {
