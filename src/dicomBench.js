d8aa7c1ce1a62ec8db02f3b56c48b0cb56d6f6f9
ivmartel <ivmartel@gmail.com>
2018-11-03 00:41:30 +0100

    Add RME. Update bench html.

--- src/dicomBench.js
+++ src/dicomBench.js
@@ -4,21 +4,46 @@ var dcmb = dcmb || {};
 var Benchmark = Benchmark || {};
 
 /**
+ * Get an html span with input text.
+ * @param {string} txt The span text.
+ * @returns {Object} A DOM span.
+ */
+dcmb.getDomSpan = function (text) {
+  var span = document.createElement("span");
+  span.appendChild(document.createTextNode(text));
+  return span;
+};
+
+/**
  * Get an html span with the difference of the two input values as a percentage.
  * @param {number} base The base number.
  * @param {number} current The number to compare to the base.
  * @returns {Object} A DOM span inculding the percentage text.
  */
-dcmb.getStatSpan = function (base, current) {
-  var span = document.createElement("span");
-  span.className = "stats";
+dcmb.getDiffSpan = function (base, current) {
   var diff = current - base;
   var sign = diff >= 0 ? "+" : "";
-  span.className += diff >= 0 ? " positive" : " negative";
-  var percent = Math.round(diff * 100 / base);
-  span.appendChild(document.createTextNode(" (" + sign + percent + "%)"));
+  var percent = diff * 100 / base;
+  var percentTxt = percent.toFixed(percent < 100 ? 2 : 0);
+
+  var span = dcmb.getDomSpan("(" + sign + percentTxt + "%)");
+  span.className = "stats";
+  span.className += diff >= 0 ? " green" : " red";
   return span;
-}
+};
+
+/**
+ * Get an html span with input Root Mean Error (RME).
+ * @param {number} rme The RME.
+ * @returns {Object} A DOM span inculding the rme text.
+ */
+dcmb.getRmeSpan = function (rme) {
+  var rmeTxt = rme.toFixed(rme < 100 ? 2 : 0);
+  var span = dcmb.getDomSpan("\u00B1" + rmeTxt + "%");
+  span.className = "stats";
+  span.className += Math.abs(rme) >= 10 ? " red" : " green";
+  return span;
+};
 
 /**
  * Insert a header row in a table with the function names as cell value.
@@ -35,7 +60,7 @@ dcmb.insertHeadRow = function (table, funcs) {
     td = hrow.insertCell();
     td.appendChild(document.createTextNode(funcs[i].name));
   }
-}
+};
 
 /**
  * Get the mean values of each columns of the input array.
@@ -48,7 +73,7 @@ dcmb.getMeans = function (results) {
   // check number of cols
   for (var i = 0; i < nrows; ++i) {
     if (results[i].length !== ncols) {
-      throw error("Different number of columns...");
+      throw new Error("Different number of columns...");
     }
   }
   // sum along columns
@@ -58,10 +83,10 @@ dcmb.getMeans = function (results) {
     for (var k = 0; k < nrows; ++k) {
       sum += results[k][j];
     }
-    means.push(sum/nrows);
+    means.push(Math.round(sum/nrows));
   }
   return means;
-}
+};
 
 /**
  * Insert a row in a table with the mean values as cell value.
@@ -78,10 +103,11 @@ dcmb.insertMeanRow = function (table, means) {
     td = hrow.insertCell();
     td.appendChild(document.createTextNode(means[i]));
     if ( i !== 0 ) {
-      td.appendChild(dcmb.getStatSpan(means[0], means[i]));
+      td.appendChild(document.createTextNode(" "));
+      td.appendChild(dcmb.getDiffSpan(means[0], means[i]));
     }
   }
-}
+};
 
 // Class to handle benchmarks.
 dcmb.DicomBench = function () {
@@ -199,9 +225,9 @@ dcmb.DicomBench = function () {
       // console output
       console.log(String(event.target));
       // html output
-      var hz = event.target.hz;
-      var opsPerSecText = hz.toFixed(hz < 100 ? 2 : 0);
-      var opsPerSec = parseInt(opsPerSecText, 10);
+      var opsPerSec = event.target.hz;
+      var opsPerSecText = opsPerSec.toFixed(opsPerSec < 100 ? 2 : 0);
+      var rme = event.target.stats.rme;
       // html
       var tName = event.target.name;
       var index = -1;
@@ -223,9 +249,11 @@ dcmb.DicomBench = function () {
       // add data to the table cell
       var cellId = index + 1;
       var cell = table.rows[dataIndex+1].cells[cellId];
-      cell.appendChild(document.createTextNode(opsPerSecText));
+      cell.appendChild(document.createTextNode(opsPerSecText + " "));
+      cell.appendChild(dcmb.getRmeSpan(rme));
       if (!isFirst) {
-        cell.appendChild(dcmb.getStatSpan(results[dataIndex][0], opsPerSec));
+        cell.appendChild(document.createTextNode(" "));
+        cell.appendChild(dcmb.getDiffSpan(results[dataIndex][0], opsPerSec));
       }
 
       // check if cancelling
