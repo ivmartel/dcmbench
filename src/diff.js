faafa93037289c35bda7bb11f6114f1fcd44b316
ivmartel <ivmartel@gmail.com>
2018-11-14 22:58:20 +0100

    Make the diff tool use the runner.

--- src/diff.js
+++ src/diff.js
@@ -1,187 +1,56 @@
 // namespace
 var dcmb = dcmb || {};
-// benchmark.js
+// diff lib
 var JsDiff = JsDiff || {};
 
-dcmb.onShowButton = function (/*id*/) {
-  // default does nothing
-};
-
-// Class to handle dump diff.
+/**
+ * Diff runner: evaluate the difference between the text results of two functions.
+ */
 dcmb.DicomDiff = function () {
 
-  // closure to self
-  var self = this;
-  // data list
-  var dataList = null;
-  // first function
-  var func1 = null;
-  // second function
-  var func2 = null;
-  // file or url
-  var isFile = null;
-  // current data index
-  var dataIndex = 0;
-  // run index
-  var runIndex = 0;
-  // status
-  var status = "ready";
-  // mean percentage
-  var meanDiffPercentage = 0;
+  // the functions to run
+  var functions = null;
 
-  // Get the status.
-  this.getStatus = function () {
-    return status;
+  /**
+   * Set the runner functions.
+   * @param {Array} funcs An array of functions in the form:
+   * {name: string, func: Object}
+   */
+  this.setFunctions = function (funcs) {
+    functions = funcs;
   };
 
-  // Set the data list.
-  this.setDataList = function (list) {
-    if ( list.length !== 0 ) {
-      dataList = list;
-      isFile = ( typeof list[0].file === "undefined" ) ? false : true;
-    }
+  /**
+   * Run the diff tool
+   * @param {Object} buffer The data buffer.
+   * @return {Array} An array of dump differences in the form:
+   * {count: number, added: boolean, removed: boolean, value: string}
+   */
+  this.run = function (buffer) {
+    // run dumps
+    var dump0 = functions[0].func(buffer);
+    var dump1 = functions[1].func(buffer);
+    // get diff
+    return JsDiff.diffChars(dump0, dump1);
   };
 
-  // Set the function pair.
-  this.setFunctionPair = function (f1, f2) {
-    func1 = f1;
-    func2 = f2;
-  };
-
-  // Set the status.
-  // @private
-  var setStatus = function (newStatus) {
-    status = newStatus;
-    // update gui
-    var pStatus = document.getElementById("diff-status");
-    pStatus.innerHTML = status;
-    var button = document.getElementById("diff-button");
-    button.disabled = false;
-    if ( self.getStatus() === "ready" ||
-      self.getStatus() === "done" ||
-      self.getStatus() === "cancelled" ) {
-      // update button
-      button.innerHTML = "Launch";
-      meanDiffPercentage = 0;
-    }
-    else if ( self.getStatus() === "running" ) {
-      // update button
-      button.innerHTML = "Cancel";
-    }
-    else if ( self.getStatus() === "cancelling" ) {
-      // disable button
-      button.disabled = true;
-    }
-  };
-
-  // Cancel the process.
-  this.cancel = function () {
-    setStatus("cancelling");
-  };
-
-  // Run the process.
-  this.run = function () {
-    var data = dataList[dataIndex];
-    // console output
-    console.log("Diff for: '" + data.name + "'");
-    // status
-    setStatus("running");
-
-    // html display
-    var preId = "diff-results-" + data.name;
-    // launch button
-    var button = document.createElement("button");
-    button.onclick = function() {
-      dcmb.onShowButton(preId);
-    };
-    button.id = preId + "-show";
-    button.appendChild(document.createTextNode(data.name));
-    // text display
-    var pre = document.createElement("pre");
-    pre.id = preId;
-    pre.style.display = "none";
-    var div = document.getElementById("diff-results");
-    // append
-    div.appendChild(button);
-    div.appendChild(pre);
-
-    // handle loaded data
-    var onloadBuffer = function (buffer) {
-      // run dumps
-      var dump1 = func1.func(buffer);
-      var dump2 = func2.func(buffer);
-      // get diff
-      var diff = JsDiff.diffChars(dump1, dump2);
-      // count
-      var nSame = 0;
-      var nDiff = 0;
-
-      diff.forEach( function (part) {
-        if ( part.added || part.removed ) {
-          nDiff += part.value.length;
-        }
-        else {
-          nSame += part.value.length;
-        }
-        // limegreen for additions, crimson for deletions
-        // grey for common parts
-        var color = part.added ? 'white' :
-          part.removed ? 'white' : 'grey';
-        var bgcolor = part.added ? 'limegreen' :
-          part.removed ? 'crimson' : 'white';
-        var span = document.createElement('span');
-        span.style.color = color;
-        span.style.background = bgcolor;
-        span.appendChild( document.createTextNode(part.value) );
-        pre.appendChild(span);
-      });
-
-      // similarity percentage
-      var total = nSame + nDiff;
-      var percent = nSame * 100 / total;
-      var diffText = document.createElement("p");
-      diffText.appendChild(document.createTextNode(percent.toPrecision(4) + "% similar."));
-      div.appendChild(diffText);
-      meanDiffPercentage += percent / dataList.length;
-
-      // check status
-      if ( self.getStatus() !== "cancelled" ) {
-        // launch next
-        ++dataIndex;
-        if ( dataIndex < dataList.length ) {
-          self.run();
-        }
-        else {
-          var meanDiffText = document.createElement("p");
-          meanDiffText.className = "diff-mean";
-          meanDiffText.appendChild(document.createTextNode("Mean: " +
-            meanDiffPercentage.toPrecision(4) + "% similar."));
-          div.appendChild(meanDiffText);
-          ++runIndex;
-          dataIndex = 0;
-          setStatus("done");
-        }
+  /**
+   * Calculate a similarity percentage.
+   * @param {Array} An array of differences as returned by JsDiff.
+   * @return {Number} The similarity percentage.
+   */
+  this.getSimiarityPercent = function (diff) {
+    // count similar and total
+    var nSame = 0;
+    var total = 0;
+    diff.forEach( function (part) {
+      if ( !part.added && !part.removed ) {
+        nSame += part.value.length;
       }
-    };
-
-    // read according to type
-    if ( isFile ) {
-      // FileReader
-      var reader = new FileReader();
-      reader.onload = function (event) {
-        onloadBuffer(event.target.result);
-      };
-      reader.readAsArrayBuffer(data.file);
-    }
-    else {
-      // XMLHttpRequest
-      var request = new XMLHttpRequest();
-      request.open('GET', data.url, true);
-      request.responseType = "arraybuffer";
-      request.onload = function (/*event*/) {
-        onloadBuffer(this.response);
-      };
-      request.send(null);
-    }
+      total += part.value.length;
+    });
+    // return percentage
+    return nSame * 100 / total;
   };
+
 };
