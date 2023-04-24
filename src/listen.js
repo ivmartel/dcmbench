08d42412ee0ebe968d185a6f646f15145b6968a5
ivmartel <ivmartel@gmail.com>
2023-04-24 11:11:45 +0200

    Fix tabs

--- src/listen.js
+++ src/listen.js
@@ -3,22 +3,22 @@ var dcmb = dcmb || {};
 dcmb.utils = dcmb.utils || {};
 
 /**
-  * ListenerHandler class: handles add/removing and firing listeners.
-  * @constructor
+ * ListenerHandler class: handles add/removing and firing listeners.
+ * @constructor
  */
 dcmb.utils.ListenerHandler = function () {
   /**
-     * listeners.
-     * @private
-     */
+   * listeners.
+   * @private
+   */
   var listeners = {};
 
   /**
-     * Add an event listener.
-     * @param {String} type The event type.
-     * @param {Object} callback The method associated with the provided
-     *  event type, will be called with the fired event.
-     */
+   * Add an event listener.
+   * @param {String} type The event type.
+   * @param {Object} callback The method associated with the provided
+   *  event type, will be called with the fired event.
+   */
   this.add = function (type, callback) {
     // create array if not present
     if (typeof listeners[type] === 'undefined') {
@@ -29,11 +29,11 @@ dcmb.utils.ListenerHandler = function () {
   };
 
   /**
-     * Remove an event listener.
-     * @param {String} type The event type.
-     * @param {Object} callback The method associated with the provided
-     *   event type.
-     */
+   * Remove an event listener.
+   * @param {String} type The event type.
+   * @param {Object} callback The method associated with the provided
+   *   event type.
+   */
   this.remove = function (type, callback) {
     // check if the type is present
     if (typeof listeners[type] === 'undefined') {
@@ -48,9 +48,9 @@ dcmb.utils.ListenerHandler = function () {
   };
 
   /**
-     * Fire an event: call all associated listeners with the input event object.
-     * @param {Object} event The event to fire.
-     */
+   * Fire an event: call all associated listeners with the input event object.
+   * @param {Object} event The event to fire.
+   */
   this.fireEvent = function (event) {
     // check if they are listeners for the event type
     if (typeof listeners[event.type] === 'undefined') {
